6aba7aca5b78b6b3e1a3b0a5168f2ad72a976c63
Jacob Maier <jacobamaier@gmail.com>
2023-03-03 11:12:29 -0800

    OrderTest: PASS Title

--- PierresTracker.Tests/ModelTests/OrderTests.cs
+++ PierresTracker.Tests/ModelTests/OrderTests.cs
@@ -8,11 +8,21 @@ namespace PierresTracker.Tests
   [TestClass]
   public class OrderTests
   {
-        [TestMethod]
+    
+    [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("Test","Test","Test","Test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    
+    
+    [TestMethod]
+    public void OrderProperties_ChecksOrderProperties_String()
+    {
+      string orderTitle = "Test Title";
+      Order newOrder = new Order(orderTitle, "Test", "Test", "Test");
+      Assert.AreEqual(orderTitle, newOrder.Title);
     }
   }
 }
--- PierresTracker/Models/Order.cs
+++ PierresTracker/Models/Order.cs
@@ -5,6 +5,17 @@ namespace PierresTracker.Models
 {
   public class Order
   {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; } // Change to int when implementing depending on time
+    public string Date { get; set; }
 
+    public Order(string orderTitle, string orderDescription, string orderPrice, string orderDate)
+    {
+      Title = orderTitle;
+      Description = orderDescription;
+      Price = orderPrice;
+      Date = orderDate;
+    }
   }
 }
