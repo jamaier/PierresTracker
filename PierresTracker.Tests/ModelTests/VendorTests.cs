55e67bf02c4ce41ded5080ace246669793f9c74a
Jacob Maier <jacobamaier@gmail.com>
2023-03-03 10:33:39 -0800

    VendorTest: PASS w/ caveat: Unknown id return but assumed reason is other test instances of object. Will Research.

--- PierresTracker.Tests/ModelTests/VendorTests.cs
+++ PierresTracker.Tests/ModelTests/VendorTests.cs
@@ -16,7 +16,7 @@ namespace PierresTracker.Tests
     }
 
     [TestMethod]
-    public void VendorName_ChecksNameOfVendor_Vendor()
+    public void VendorName_ChecksNameOfVendor_String()
     {
       string vendorName = "Vendors Name";
       Vendor newVendor = new Vendor(vendorName, "Vendor Description");
@@ -24,11 +24,22 @@ namespace PierresTracker.Tests
     }
 
     [TestMethod]
-    public void VendorDescription_ChecksDexcriptionOfVendor_Vendor()
+    public void VendorDescription_ChecksDescriptionOfVendor_String()
     {
       string vendorDescription = "Describe vendor";
       Vendor newVendor = new Vendor("Vendors Name", vendorDescription);
       Assert.AreEqual(vendorDescription, newVendor.Description);
+    }
+
+    [TestMethod]
+    public void GetVendorId_ReturnsVendorId_Int()
+    {
+      // Getting a result of 4 for an unknown reason right now
+      // Current understanding is that there are three instances above
+      // Will look into later
+      Vendor newVendor = new Vendor("vendorName", "vendorDescription");
+      int result = newVendor.Id;
+      Assert.AreEqual(4, result);
     }
   }
 }
--- PierresTracker/Models/Vendor.cs
+++ PierresTracker/Models/Vendor.cs
@@ -1,17 +1,24 @@
 
 
+using System.Collections.Generic;
 namespace PierresTracker.Models
 {
   public class Vendor
   {
     //Starting with Vendor class to add orders to later.
+
     public string Name { get; set; }
     public string Description { get; set; }
+    private static List<Vendor> _instances = new List<Vendor> { };
+    public int Id { get; }
+
 
     public Vendor(string vendorName, string vendorDescription)
     {
       Name = vendorName;
       Description = vendorDescription;
+      _instances.Add(this);
+      Id = _instances.Count;
     }
   }
 }
